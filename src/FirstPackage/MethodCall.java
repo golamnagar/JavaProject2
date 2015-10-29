a55a9709bee633561085d30926258e282162c060
golamnagar <golam1999@gmail.com>
2015-10-29 17:29:03 -0400

    Update code

--- src/FirstPackage/MethodCall.java
+++ src/FirstPackage/MethodCall.java
@@ -5,10 +5,22 @@ public class MethodCall {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   print();
+  sum();
 	}
 
 	public static void print() {
 		
 		System.out.println("I am in the city");
+	}
+	
+	public static void sum(){
+		
+		int a = 12;
+		int b = 13;
+		int sum;
+		
+		sum = a + b;
+		
+		System.out.println(sum);
 	}
 }
