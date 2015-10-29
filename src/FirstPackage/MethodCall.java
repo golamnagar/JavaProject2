97f47f264208a08df404c8e29af2ffbecbd6b1db
golamnagar <golam1999@gmail.com>
2015-10-29 17:20:19 -0400

    First Upload

---
+++ src/FirstPackage/MethodCall.java
@@ -0,0 +1,14 @@
+package FirstPackage;
+
+public class MethodCall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  print();
+	}
+
+	public static void print() {
+		
+		System.out.println("I am in the city");
+	}
+}
